04f4590fe66b7f73ae242dc9d8255525e9525832
jstiven01 <stiven10@gmail.com>
2019-10-07 14:50:44 -0500

    Formatting Stickler changes

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,35 +1,37 @@
 # frozen_string_literal: true
+
 # Project 1: Bubble Sort
 # bubble_sort: Takes an array and returns a sorted array using the bubble sort methodology
 def bubble_sort(arr)
+  swap = true
+  while swap
+    swap = false
+    for i in 1...arr.length do
+      if arr[i-1] > arr[i]
+        temp = arr[i]
+        arr[i] = arr[i-1]
+        arr[i-1] = temp
+        swap = true
+      end
+    end
+  end
+  arr
+end
+
+# bubble_sort_by: Takes an array and returns a sorted array using the bubble sort methodology 
+# and the condition of the block
+def bubble_sort_by(arr)
     swap = true
     while swap
       swap = false
       for i in 1...arr.length() do
-          if arr[i-1] > arr[i]
-              temp = arr[i]
-              arr[i] = arr[i-1]
-              arr[i-1] = temp
-              swap = true
-          end
+        if yield(arr[i-1], arr[i]).positive?
+          temp = arr[i]
+          arr[i] = arr[i-1]
+          arr[i-1] = temp
+          swap = true
+        end
       end
     end
-    return arr
-end
-
-# bubble_sort_by: Takes an array and returns a sorted array using the bubble sort methodology and the condition of the block
-def bubble_sort_by(arr)
-    swap = true
-    while swap
-       swap = false
-       for i in 1...arr.length() do
-          if yield(arr[i-1], arr[i]).positive?
-              temp = arr[i]
-              arr[i] = arr[i-1]
-              arr[i-1] = temp
-              swap = true
-          end
-       end
-    end
-    return arr
+    arr
 end
