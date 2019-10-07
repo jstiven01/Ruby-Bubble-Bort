25ae46545e26e089ef7179b0fabcf02b5eae1f95
jstiven01 <stiven10@gmail.com>
2019-10-07 13:28:25 -0500

    Formatting with Stickler suggestions

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,19 @@
 # Project 1: Bubble Sort
-# bubble_sort: Takes an array and returns a sorted array using the bubble sort methodology 
+# bubble_sort: Takes an array and returns a sorted array using the bubble sort methodology
 def bubble_sort(arr)
-	swap = true
-	while swap
-	  swap = false
-	  for i in 1...arr.length() do
-		  if arr[i-1] > arr[i]
-			  temp = arr[i]
-			  arr[i] = arr[i-1]
-			  arr[i-1] = temp
-			  swap = true
-		  end
-	  end
-	end
-	return arr
+	 swap = true
+	 while swap
+	   swap = false
+	   for i in 1...arr.length() do
+		   if arr[i-1] > arr[i]
+			   temp = arr[i]
+			   arr[i] = arr[i-1]
+			   arr[i-1] = temp
+			   swap = true
+		   end
+	   end
+	 end
+	 return arr
 end
 
 puts bubble_sort([4,3,78,2,0,2])
