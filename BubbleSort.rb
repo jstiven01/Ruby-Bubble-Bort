285fe7d52868b96f2b5907a3e03841de2c3ba23b
Luis Preza <preza.luis@gmail.com>
2019-10-07 12:37:44 -0500

    optimized buble sort

--- BubbleSort.rb
+++ BubbleSort.rb
@@ -1,1 +1,19 @@
 #Project 1: Bubble Sort
+def bubble_sort(arr)
+	swap = true
+
+	while swap
+		swap = false
+		for i in 1...arr.length() do
+			if arr[i-1] > arr[i]
+				temp = arr[i]
+				arr[i] = arr[i-1]
+				arr[i-1] = temp
+				swap = true
+			end
+		end
+	end
+
+	return arr
+	
+end
