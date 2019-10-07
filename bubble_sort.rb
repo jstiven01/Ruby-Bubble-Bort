63de461b92a44089282a9d81be2c67d4cb2213d3
jstiven01 <stiven10@gmail.com>
2019-10-07 14:38:45 -0500

    Formatting Stickler Errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,47 +1,35 @@
+# frozen_string_literal: true
 # Project 1: Bubble Sort
 # bubble_sort: Takes an array and returns a sorted array using the bubble sort methodology
 def bubble_sort(arr)
-	 swap = true
-	 while swap
-	   swap = false
-	   for i in 1...arr.length() do
-		   if arr[i-1] > arr[i]
-			   temp = arr[i]
-			   arr[i] = arr[i-1]
-			   arr[i-1] = temp
-			   swap = true
-		   end
-	   end
-	 end
-	 return arr
-end
-
-puts bubble_sort([4,3,78,2,0,2])
-
-# bubble_sort_by: Takes an array and returns a sorted array using the bubble sort methodology and the condition of the block
-def bubble_sort_by(arr)
-  swap = true
-	while swap
-		swap = false
-		for i in 1...arr.length() do
-			if yield(arr[i-1], arr[i]) > 0
-				temp = arr[i]
-				arr[i] = arr[i-1]
-				arr[i-1] = temp
-				swap = true
-			end
-		end
-	end
+    swap = true
+    while swap
+      swap = false
+      for i in 1...arr.length() do
+          if arr[i-1] > arr[i]
+              temp = arr[i]
+              arr[i] = arr[i-1]
+              arr[i-1] = temp
+              swap = true
+          end
+      end
+    end
     return arr
 end
 
-sortedarray = bubble_sort_by(["hi","hello","hey"]) do |left, right|
-  left.length - right.length
+# bubble_sort_by: Takes an array and returns a sorted array using the bubble sort methodology and the condition of the block
+def bubble_sort_by(arr)
+    swap = true
+    while swap
+       swap = false
+       for i in 1...arr.length() do
+          if yield(arr[i-1], arr[i]).positive?
+              temp = arr[i]
+              arr[i] = arr[i-1]
+              arr[i-1] = temp
+              swap = true
+          end
+       end
+    end
+    return arr
 end
-puts sortedarray
-
-sortedarray = bubble_sort_by([5,3,2]) do |left, right|
-  left - right
-end
-
-puts sortedarray
